27d10c04a50e2455a041e4e39b0b52f1e66e31b7
CongNM <nguyenmanhcongzzz@gmail.com>
2026-07-21 17:01:05 +0700

    sys prog

--- SystemProgramming/ReadFile.cpp
+++ SystemProgramming/ReadFile.cpp
@@ -4,24 +4,46 @@
 using namespace std;
 
 int main() {
+	// Create a file
 	HANDLE hFile;
-	hFile = CreateFile
-	(
-		L"D:\\Work\\NCS\\CheckAns\\newFile4.txt", 
-		GENERIC_READ | GENERIC_WRITE, 
-		0, 
-		NULL, 
-		CREATE_NEW, 
-		FILE_ATTRIBUTE_NORMAL, 
-		NULL 
-	);
-	bool bRFile;
-	bRFile = ReadFile
-	(
-		hFile, 
-		"Hello World", 
-		11, 
-		NULL, 
+	hFile = CreateFile(
+		L"D:\\Work\\NCS\\CheckAns\\newFile4.txt",
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_NEW,
+		FILE_ATTRIBUTE_NORMAL,
 		NULL
 	);
+
+	// initialize data to write
+	char chBuffer[] = "Hello World";
+	DWORD dwBytesToWritte = strlen(chBuffer);
+	DWORD dwBytesWritten = 0;
+	DWORD dwBytesToRead = 0;
+	DWORD dwBytesRead = 0;
+
+	// Write to file
+	bool bWFile = WriteFile(
+		hFile,
+		chBuffer,
+		dwBytesToWritte,
+		&dwBytesWritten,
+		NULL
+	);
+
+	// Read from file
+	bool bRFile = ReadFile(
+		hFile,
+		chBuffer,
+		dwBytesToRead,
+		&dwBytesRead,
+		NULL
+	);
+
+	// Print the read data
+	cout << "Data read from file: " << chBuffer << endl;
+
+	// Close HANDLE
+	CloseHandle(hFile);
 }
